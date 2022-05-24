80f07dca2ecc74b0b1462b9ab3b873949656ad2f
FlutLab.io <>
2022-05-24 18:46:04 +0000

    Autosave

--- lib/screens/add_review/add_review_screen.dart
+++ lib/screens/add_review/add_review_screen.dart
@@ -9,8 +9,7 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 class AddReviewScreen extends StatefulWidget {
   final dynamic data;
   final int type;
-  const AddReviewScreen({Key? key, required this.data, required this.type})
-      : super(key: key);
+  const AddReviewScreen({Key? key, required this.data, required this.type}) : super(key: key);
 
   @override
   _AddReviewScreenState createState() => _AddReviewScreenState();
@@ -26,25 +25,14 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
     super.initState();
   }
 
-  void _saveData(Map<String, dynamic> value, BuildContext context, dynamic data,
-      int type) {
-    dynamic review = PlaceReview(
-        objectName: data.name,
-        rating: _ratingValue,
-        type: 0,
-        comment: value["description"] ?? "",
-        id: data.id);
+  void _saveData(Map<String, dynamic> value, BuildContext context, dynamic data, int type) {
+    dynamic review = PlaceReview(objectName: data.name, rating: _ratingValue, type: 0, comment: value["description"] ?? "", id: data.id);
 
     if (type == 2) {
-      review = AccomodationReview(
-          objectName: data.name,
-          rating: _ratingValue,
-          type: 2,
-          comment: value["description"] ?? "",
-          id: data.id);
+      review = AccomodationReview(objectName: data.name, rating: _ratingValue, type: 2, comment: value["description"] ?? "", id: data.id);
     }
 
-    Provider.of<ReviewProvider>(context, listen: false).addReview(review);
+    Provider.of<ReviewProvider>(context, listen: true).addReview(review);
     Navigator.of(context).pop();
   }
 
@@ -59,51 +47,48 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
               child: Container(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    FormBuilder(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          RatingBar(
-                              initialRating: 0,
-                              direction: Axis.horizontal,
-                              allowHalfRating: true,
-                              itemCount: 5,
-                              ratingWidget: RatingWidget(
-                                  full: const Icon(Icons.star,
-                                      color: Colors.orange),
-                                  half: const Icon(
-                                    Icons.star_half,
-                                    color: Colors.orange,
-                                  ),
-                                  empty: const Icon(
-                                    Icons.star_outline,
-                                    color: Colors.orange,
-                                  )),
-                              onRatingUpdate: (value) {
-                                setState(() {
-                                  _ratingValue = value;
-                                });
-                              }),
-                          SizedBox(height: 20),
-                          FormBuilderTextField(
-                            name: 'description',
-                            decoration: const InputDecoration(
-                              labelText: 'Review',
-                            ),
-                            onChanged: (value) {
-                              setState(() {
-                                _description = value;
-                              });
-                            },
-                            validator: FormBuilderValidators.compose([]),
-                          ),
-                        ],
+              child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                FormBuilder(
+                  key: _formKey,
+                  child: Column(
+                    children: <Widget>[
+                      RatingBar(
+                          initialRating: 0,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          ratingWidget: RatingWidget(
+                              full: const Icon(Icons.star, color: Colors.orange),
+                              half: const Icon(
+                                Icons.star_half,
+                                color: Colors.orange,
+                              ),
+                              empty: const Icon(
+                                Icons.star_outline,
+                                color: Colors.orange,
+                              )),
+                          onRatingUpdate: (value) {
+                            setState(() {
+                              _ratingValue = value;
+                            });
+                          }),
+                      SizedBox(height: 20),
+                      FormBuilderTextField(
+                        name: 'description',
+                        decoration: const InputDecoration(
+                          labelText: 'Review',
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _description = value;
+                          });
+                        },
+                        validator: FormBuilderValidators.compose([]),
                       ),
-                    ),
-                  ]),
+                    ],
+                  ),
+                ),
+              ]),
             ),
           )),
           Container(
@@ -120,26 +105,19 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
                     width: 100,
                     child: const Text(
                       'Close',
-                      style: TextStyle(
-                          color: AppColors.kPrimaryColor,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 12),
+                      style: TextStyle(color: AppColors.kPrimaryColor, fontWeight: FontWeight.w700, fontSize: 12),
                     ),
                     alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: AppColors.kWhite,
-                        borderRadius: BorderRadius.circular(10)),
+                    decoration: BoxDecoration(color: AppColors.kWhite, borderRadius: BorderRadius.circular(10)),
                   ),
                 ),
                 InkWell(
                   onTap: () {
                     _formKey.currentState!.save();
                     if (_formKey.currentState!.validate()) {
-                      _saveData(_formKey.currentState!.value, context,
-                          widget.data, widget.type);
+                      _saveData(_formKey.currentState!.value, context, widget.data, widget.type);
                     } else {
-                      var snackBar =
-                          SnackBar(content: Text('Validation failed'));
+                      var snackBar = SnackBar(content: Text('Validation failed'));
                       ScaffoldMessenger.of(context).showSnackBar(snackBar);
                     }
                   },
@@ -148,15 +126,10 @@ class _AddReviewScreenState extends State<AddReviewScreen> {
                     width: 100,
                     child: const Text(
                       'Add review',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 12),
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700, fontSize: 12),
                     ),
                     alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: AppColors.kPrimaryColor,
-                        borderRadius: BorderRadius.circular(10)),
+                    decoration: BoxDecoration(color: AppColors.kPrimaryColor, borderRadius: BorderRadius.circular(10)),
                   ),
                 )
               ],
